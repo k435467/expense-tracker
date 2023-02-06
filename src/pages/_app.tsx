5436d2c1f72fcbdf832dbe9ed5a6e8098144d6d3
Ethan Guo <k435467@gmail.com>
2023-02-07 00:39:10 +0800

    add tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import '@/styles/globals.css'
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { AuthUserProvider } from "@/components/auth";
+import { Container } from "@/components/container";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthUserProvider>
-      <Component {...pageProps} />
+      <Head>
+        <title>Expense Tracker</title>
+        <meta name="description" content="A expense tracker web app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </AuthUserProvider>
   );
 }
